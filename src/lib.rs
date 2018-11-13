7d19f44fa291e496248a0576b4c5998f8c5fc323
Jonathan Stites <mail@jonstites.com>
2018-11-12 20:30:06 -0600

    added iterative action scores for comparison

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
 #![allow(dead_code, unused_variables, unused_parens, unused_imports)]
 #![feature(nll)]
+#![feature(proc_macro_hygiene)]
+#[macro_use] extern crate flamer;
 
 extern crate itertools;
+
+extern crate flame;
 
 use itertools::Itertools;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::slice::Iter;
 
 mod states;
@@ -43,7 +48,7 @@ pub struct ActionScores {
 }
 
 impl ActionScores {
-
+    #[flame("action_scores")]
     pub fn new() -> ActionScores {
         let mut rolls = Vec::new();
         for dice_number in 0..=DICE_TO_ROLL {
@@ -55,32 +60,82 @@ impl ActionScores {
         
         let state_values = HashMap::new();
         
-        let mut action_scores = ActionScores {
+        ActionScores {
             state_values,
             rolls,
             possible_rolls
-        };
-        let mut starting_state = State::default();
-        starting_state.entries_taken = [true; 13];
-        for i in 0..6 {
-            starting_state.entries_taken[i] = false;
         }
+    }
+
+    pub fn run(&mut self, starting_state: State)  {
+        /*let mut starting_state = State::default();
+        starting_state.entries_taken = [true; 13];
+        for i in 0..2 {
+            starting_state.entries_taken[i] = false;
+        }*/
         
-        action_scores.set_scores(starting_state);
-        println!("{:?}", action_scores.state_values.get(&starting_state));
-        action_scores            
+        //self.set_scores(starting_state);
+        let mut states = self.children(starting_state);
+        while let Some(state) = states.pop() {
+            self.set_score(state);
+        }
+        println!("{:?}", self.state_values.get(&starting_state));
     }
 
     pub fn num_states(&self) -> usize {
         self.state_values.keys().len()
     }
+    
+    #[flame]
+    pub fn children(&self, starting_state: State) -> Vec<State> {
+        let mut queue = VecDeque::new();
+        queue.push_back(starting_state);
+        
+        let mut added = HashSet::new();
+        added.insert(starting_state);
+        let mut states = Vec::new();
 
-    fn set_scores(&mut self, state: State) {
+        while let Some(state) = queue.pop_front() {
+            states.push(state);
+            for entry in Entry::iterator().filter(|&e| state.is_valid(&e)) {
+                for roll in self.possible_rolls.clone() {
+                    let child = state.child(*entry, roll);
+                    if !added.contains(&child) {
+                        queue.push_back(child);
+                        added.insert(child);                        
+                    }
+                }
+            }
+        }
+        states
+    }
+
+
+    pub fn set_score(&mut self, state: State) {
+        let default_full_state = Fs::I(
+            FullState {
+                dice: DiceCombination::new(),
+                rolls_remaining: 3
+            });
+
+        let score =  FullStateCalculator {
+            minimal_state: &state,
+            minimal_state_values: &self.state_values,
+            possible_rolls: &self.possible_rolls,
+            roll_probs: &self.rolls,
+            full_state_values: HashMap::new()
+        }.full_state_calculation(default_full_state);
+        self.state_values.insert(state, score);
+    }
+    
+    #[flame("as")]
+    pub fn set_scores(&mut self, state: State) {
+
         if self.state_values.contains_key(&state) {
             return;
         }
         
-        for entry in Entry::iterator().filter(|&e| state.is_valid(*e)) {
+        for entry in Entry::iterator().filter(|&e| state.is_valid(&e)) {
             for roll in self.possible_rolls.clone() {
                 self.set_scores(state.child(*entry, roll));
             }
@@ -101,33 +156,34 @@ impl ActionScores {
         }.full_state_calculation(default_full_state);
         self.state_values.insert(state, score);
     }
+
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
-struct State {
-    entries_taken: [bool; 13],
+pub struct State {
+    pub entries_taken: [bool; 13],
     positive_yahtzee: bool,
     upper_score_total: i32,    
 }
 
 impl State {
 
-    fn default() -> State {
+    pub fn default() -> State {
         State {
             entries_taken: [false; 13],
             positive_yahtzee: false,
             upper_score_total: 0,
         }
     }
-
-    fn is_valid(&self, entry: Entry) -> bool {
-        !self.entries_taken[State::index(entry)]
+    //#[flame]
+    fn is_valid(&self, entry: &Entry) -> bool {
+        !self.entries_taken[*entry as usize]
     }
-    
+    //#[flame]
     fn child(&self, entry: Entry, roll: DiceCombination) -> State {
         let mut entries_taken = self.entries_taken.clone();
 
-        let index = State::index(entry);
+        let index = entry as usize;
         entries_taken[index] = true;     
 
         let upper_score_total = self.new_upper_score(entry, roll);
@@ -139,9 +195,8 @@ impl State {
             positive_yahtzee
         }
     }
-
     fn new_upper_score(&self, entry: Entry, roll: DiceCombination) -> i32 {
-        let index = State::index(entry);
+        let index = entry as usize;
         let additional_upper_score = match entry {
             Ones => 1 * roll.dice[index],
             Twos => 2 * roll.dice[index],
@@ -180,12 +235,12 @@ impl State {
     fn score_dice(&self, entry: Entry, roll: DiceCombination) -> i32 {
         // implemented as free choice joker rule
         // todo: support configuration for joker rule
-        let index = State::index(entry);
+        let index = entry as usize;
         
         let yahtzee_index = roll.dice.iter().position(|&x| x == 5);
             
         let joker = roll.is_yahtzee() &&
-                self.entries_taken[State::index(Yahtzee)] &&
+                self.entries_taken[Yahtzee as usize] &&
                 self.entries_taken[yahtzee_index.unwrap()];
 
 
@@ -263,24 +318,6 @@ impl State {
     fn is_terminal(&self) -> bool {
         self.entries_taken.iter().all(|&x| x)
     }
-
-    fn index(entry: Entry) -> usize {
-        match entry {
-            Ones => 0,
-            Twos => 1,
-            Threes => 2,
-            Fours => 3,
-            Fives => 4,
-            Sixes => 5,
-            ThreeOfAKind => 6,
-            FourOfAKind => 7,
-            FullHouse => 8,
-            SmallStraight => 9,
-            LargeStraight => 10,
-            Yahtzee => 11,
-            Chance => 12,
-        }
-    }    
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
@@ -304,15 +341,16 @@ struct FullStateCalculator<'a> {
 }
 
 impl<'a> FullStateCalculator<'a> {
+    #[flame]
     fn best_entry_score(&self, full_state: &FullState) -> f64 {
 
         Entry::iterator()
-            .filter(|&e| self.minimal_state.is_valid(*e))                
+            .filter(|&e| self.minimal_state.is_valid(&e))                
             .map(|&e| self.minimal_state.score(e, full_state.dice) as f64 + self.minimal_state_values.get(&self.minimal_state.child(e, full_state.dice)).unwrap())
             .fold(std::f64::NAN, f64::max) // Find the largest non-NaN in vector, or NaN otherwise
         
 }
-
+    #[flame]
     fn average_rolled_dice_score(
         &mut self,
         full_state: &FullState,
@@ -330,7 +368,7 @@ impl<'a> FullStateCalculator<'a> {
         }
         expected_value
     }
-
+    #[flame]
     fn best_keeper_score(
         &mut self,
         full_state: &FullState,
@@ -341,10 +379,8 @@ impl<'a> FullStateCalculator<'a> {
             .map(|new_fs| self.full_state_calculation(new_fs))            
             .fold(std::f64::NAN, f64::max) // Find the largest non-NaN in vector, or NaN otherwise
     }
-
-    fn full_state_calculation(
-        &mut self,
-        full_state: Fs) -> f64 {
+    //#[flame]
+    fn full_state_calculation(&mut self, full_state: Fs) -> f64 {
 
         if self.minimal_state.is_terminal() {
             return 0.0;
@@ -492,7 +528,7 @@ enum Entry {
 }
 
 impl Entry {
-    
+
     fn iterator() -> Iter<'static, Entry> {
         static ENTRIES: [Entry;  13] = [
             Ones,
@@ -513,243 +549,7 @@ impl Entry {
     }
 
 }
-/*
-struct Advisor {
-    config: Config,
-    // A lookup table where the first key is the number of dice rolled
-    // and the value is another lookup of vector of dice counts to the
-    // probability.
-    roll_probabilities: Vec<HashMap<DiceCombination, f64>>,
-    expected_values: HashMap<State, f64>
-}
 
-impl Advisor {
-    
-    fn new() -> Advisor {
-        Advisor {
-            config: Config::default(),
-            roll_probabilities: Vec::new(),
-            expected_values: HashMap::new(),
-        }
-    }
-    
-    fn with_options(config: Config) -> Advisor {
-
-        Advisor {
-            config,
-            roll_probabilities: Vec::new(),
-            expected_values: HashMap::new(),            
-        }
-    }
-
-    fn run(&mut self) {
-        self.set_roll_probabilities();
-        self.set_expected_values()
-    }
-
-    fn set_roll_probabilities(&mut self) {
-        for dice_number in 0..=DICE_TO_ROLL {
-            let probabilities = DiceCombination::probabilities(dice_number);
-            self.roll_probabilities.push(probabilities);
-        }
-    }
-
-    fn set_expected_values(&mut self) {
-        let mut stack = vec!(State::default());
-        let mut seen: HashSet<State> = HashSet::new();
-        seen.insert(State::default());
-
-        while let Some(state) = stack.pop() {
-            let mut unseen_children: Vec<_> = state.children()
-                .into_iter()
-                .filter(|child| !seen.contains(child))
-                .collect();
-
-            if unseen_children.len() == 0 {
-                let mut widget = Widget::new(state);
-                self.expected_values.insert(state, widget.run());
-                                            
-            } else {
-                stack.push(state);
-                for child in unseen_children.iter().cloned() {
-                    seen.insert(child);
-                    stack.push(child);
-                }
-            }
-            println!("values {:?}", self.expected_values);
-        }
-    }
-}
-
-struct Widget {
-    parent: State,
-    local_state_values: HashMap<WidgetState, f64>,
-    child_values: HashMap<State, f64>,
-}
-
-impl Widget {
-
-    fn new(state: State) -> Widget {
-        Widget {
-            parent: state,
-            local_state_values: HashMap::new(),
-            child_values: HashMap::new(),
-        }
-    }
-    
-    fn run(&mut self) -> f64 {
-        let widget_state = WidgetState::Roll(LocalState::default());
-        self.widget(widget_state);
-
-        println!("{:?} local widget cache: {:?}", self.local_state_values, widget_state);
-        5.7
-        //*self.local_state_values.get(&widget_state).unwrap()
-    }
-
-    fn widget(&mut self, widget_state: WidgetState) {
-
-        match widget_state {
-            WidgetState::Child(state) => {}, //widget_state.insert(*self.child_values.get(&state).unwrap(),
-            WidgetState::Keeper(local_state) => {}, //5.0,
-                /*for roll in local_state.make_roll() \\
-                total roll = roll + dice;
-                new_state = make new state;
-                something something
-                 */
-
-            WidgetState::Roll(local_state) => {
-
-            } //4.0,
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
-struct DiceCombination {
-    dice: [i32; DICE_SIDES],
-}
-
-impl DiceCombination {
-
-    fn new() -> DiceCombination {
-
-        DiceCombination {
-            dice: [0; DICE_SIDES],
-        }
-    }
-
-    fn from_permutation(permutation: Vec<i32>) -> DiceCombination {
-        let mut combination = [0; DICE_SIDES];
-        for die in permutation {
-            let index: usize = (die - 1) as usize;
-            let mut count = combination[index];
-            combination[index] = count + 1;
-        }
-        
-        DiceCombination {
-            dice: combination,
-        }
-    }
-
-    fn probabilities(
-        dice_number: i32
-    ) -> HashMap<DiceCombination, f64> {
-
-        // Special case to handle the ability to "roll" 0 dice.
-        if dice_number == 0 {
-            let mut probabilities = HashMap::new();
-            probabilities.insert(DiceCombination::new(), 1.0);
-            return probabilities;
-        }
-
-        let divisor = (DICE_SIDES as f64).powf(dice_number as f64);
-        let mut probabilities = HashMap::new();
-
-        let permutation_it = (0..dice_number)
-            .map(|_| 1..=(DICE_SIDES as i32))
-            .multi_cartesian_product();
-
-        for permutation in permutation_it {
-            let combination = DiceCombination::from_permutation(permutation);
-                
-            let probability = probabilities
-                .entry(combination)
-                .or_insert(0.0);
-
-            *probability += 1.0 / divisor;
-        }
-        probabilities
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
-struct State {
-    entries_taken: [bool; 13],
-    positive_yahtzee: bool,
-    upper_score_total: i32,    
-}
-
-impl State {
-
-    fn default() -> State {
-        State {
-            entries_taken: [false; 13],
-            positive_yahtzee: false,
-            upper_score_total: 0,
-        }
-    }
-
-    fn children(&self) -> Vec<State> {
-        let mut child = State::default();
-        child.upper_score_total = 100;
-        vec!(child)
-    }
-
-    fn child(&self, dice: DiceCombination, entry: Entry) {
-
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
-struct LocalState {
-    rolls_taken: i32,
-    dice: DiceCombination,
-}
-
-impl LocalState {
-
-    fn default() -> LocalState {
-        LocalState {
-            rolls_taken: 0,
-            dice: DiceCombination::new(),
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
-enum WidgetState {
-    Child(State),
-    Keeper(LocalState),
-    Roll(LocalState),
-}
-
-enum Entry {
-    Ones,
-    Twos,
-    Threes,
-    Fours,
-    Fives,
-    Sixes,    
-    ThreeOfAKind,
-    FourOfAKind,
-    FullHouse,
-    SmallStraight,
-    LargeStraight,
-    Yahtzee,
-    Chance
-}
-*/
-*/
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -772,13 +572,25 @@ mod tests {
         let actual_dice_combinations = probabilities.len();
         let expected_dice_combinations = 252;
         assert_eq!(actual_dice_combinations, expected_dice_combinations);
-
         
         let total_prob: f64 = probabilities.values().into_iter().sum();
         let abs_difference = (total_prob - 1.0).abs();
         let tolerance = 0.0000001;
         assert!(abs_difference < tolerance);
     }
+
+    #[test]
+    fn test_correct_children() {
+        let action_scores = ActionScores::new();
+        let mut starting_state = State::default();
+        for i in 0..5 {
+            starting_state.entries_taken[i] = true;
+        }
+        let children = action_scores.children(starting_state);
+
+        assert_eq!(1344, children.len());
+    }
+    
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,35 @@
 extern crate optizee;
+use std::fs::File;
+extern crate flame;
+extern crate time;
 
+use time::PreciseTime;
 
 fn main() {
-    let action_scores = optizee::ActionScores::new();
+    let mut action_scores = optizee::ActionScores::new();
+    let mut state = optizee::State::default();
+    for i in 0..4 {
+        state.entries_taken[i] = true;
+    };
+
+    let start = PreciseTime::now();
+    let states = action_scores.children(state);
+    println!("number of states: {:?}", states.len());
+    let end = PreciseTime::now();
+    println!("{} seconds for children.", start.to(end));
+
+    //println!("{:?}", states);
+    let start = PreciseTime::now();
+    action_scores.run(state);//optizee::State::default());
     println!("number of states: {:?}", action_scores.num_states());
-    //action_scores.set_scores(optizee::State::default());
+    let end = PreciseTime::now();
+    println!("{} seconds for iterative action scores.", start.to(end));
+
+    let mut action_scores = optizee::ActionScores::new();
+    let start = PreciseTime::now();
+    action_scores.set_scores(state);//optizee::State::default());
+    println!("number of states: {:?}", action_scores.num_states());
+    let end = PreciseTime::now();
+    println!("{} seconds for recursive action scores.", start.to(end));
+
 }
